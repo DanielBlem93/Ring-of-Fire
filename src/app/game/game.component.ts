57fc621edb3f815d0823656ce05191d72bafdb92
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2023-10-26 12:37:55 +0200

    added card anaimations

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+
+
+export class GameComponent {
+
+  pickCardAnimation = false
+
+
+
+
+  constructor() {
+
+  }
+
+
+
+  takeCard() {
+    this.pickCardAnimation = true
+  }
+}
